dd3b6d3ff15dfd6f23c584febe3b2a632cfef21b
bhavya162 <bhavyasharma0558@gmail.com>
2025-08-29 10:48:31 +0530

    Delete html directory

--- html/index.js
+++
@@ -1,5 +0,0 @@
-function call(){
-    let message = document.getElementById("name").value;
-    console.log('hello '+ message);
-    document.getElementById('output').innerText = 'hello ' + message;
-}
